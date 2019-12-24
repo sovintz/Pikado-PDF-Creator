6000d8d7065818f308abb90c65e854c75749ef1d
lansovinc <lan.sovinc@gmail.com>
2019-12-24 23:35:30 +0100

    PDFkit pack test 2

--- create_pdf.py
+++ create_pdf.py
@@ -7,7 +7,8 @@ def create_pdf(title, offer, type_o, size, size_l, year, price, short, long, ima
     if platform.system() == 'Windows':
         config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
     else:
-        config = pdfkit.configuration(wkhtmltopdf='wkhtmltopdf-pack')
+        config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+    #config = pdfkit.configuration(wkhtmltopdf='wkhtmltopdf-pack-ng')
     options = {
         'page-size': 'A4',
         'encoding': "UTF-8",
