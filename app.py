1077b0e8e83bcf2d9169303e50a4e31f83d933e1
sovaCode <lan.sovinc@gmail.com>
2020-01-29 15:38:37 +0100

    Font weights adjusted

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
-import pdfkit
 from flask_weasyprint import render_pdf
 
-from create_pdf import create_pdf
 from get_listing_data import get_listing_data
-from img_to_base64 import img_to_base64
 from weasyprint import HTML
 
 app = Flask(__name__)
@@ -41,10 +38,8 @@ def pdf():
         short = request.form["short"]
         long = request.form["long"]
         images_array = request.form.getlist("image")
-        # create base64 images
-        #images_array_base64 = img_to_base64(images_array)
         # render the PDF
-        html = render_template('pdf_template.html', title=title, offer=offer, type_o=type_o, size=size, size_l=size_l, year=year, price=price, short=short, long=long, images_array_base64=images_array)
+        html = render_template('pdf_template.html', title=title, offer=offer, type_o=type_o, size=size, size_l=size_l, year=year, price=price, short=short, long=long, images_array=images_array)
         #html = render_template('report.html')
         return render_pdf(HTML(string=html))
         #return create_pdf(title=title, offer=offer, type_o=type_o, size=size, size_l=size_l, year=year, price=price, short=short, long=long, images_array_base64=images_array_base64)
